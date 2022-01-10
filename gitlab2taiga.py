36c493a5914faee4bd4e59645c9f789dcfe88d48
Mikel Cordovilla <mikel.cordovilla@tegonal.com>
2022-01-10 16:34:58 +0100

    Add gettoken function

--- gitlab2taiga.py
+++ gitlab2taiga.py
@@ -1,43 +1,45 @@
+#!/usr/bin/env python3
+
 import ujson
 import pandas as pd
 import requests
 import json
 import sys, getopt
 import getpass
+import validators
 
-# endpoint mandatory
+
+ENDPOINT_GET_PROJECT = "/api/v1/projects"
+ENDPOINT_LOGIN = "/api/v1/auth"
+
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"h:i:m:e:u",["="])
+        opts, args = getopt.getopt(argv,'hi:m:e:u:')
     except getopt.GetoptError:
         print ('gitlab2taiga.py -i <issues file> -m <members file> -e <taiga endpoint> -u <taiga username>')
         sys.exit(2)
-    if not opts:
-        print ('gitlab2taiga.py')
+    if len(opts) != 4:
+        print ('gitlab2taiga.py -i <issues file> -m <members file> -e <taiga endpoint> -u <taiga username>')
+        sys.exit(2)
     else:
+        print (opts)
         for opt, arg in opts:
             if opt == '-h':
                 print ('gitlab2taiga.py -i <issues file> -m <members file> -e <taiga endpoint> -u <taiga username>')
                 sys.exit()
             elif opt in '-i':
-                if (arg != null)
-                    #importIssues(arg)
-                    print ('issues file : ', arg)
+                issues_file = arg
             elif opt in '-m':
-                if (arg != null)
-                    #importIssues(arg)
-                    print ('members file : ', arg)
+                members_file = arg
             elif opt in '-e':
-                if (arg != null)
-                    #importIssues(arg)
-                    print ('endpoint : ', arg)
+                endpoint = arg
             elif opt in '-u':
-                if (arg != null)
-                    #importIssues(arg)
-                    print ('usename : ', arg)
+                username = arg
             else:
                 print ('gitlab2taiga.py -i <issues file> -m <members file> -e <taiga endpoint> -u <taiga username>')
+        if (username is not None) and (issues_file is not None) and (members_file is not None) and (endpoint is not None):
+            getAccessToken(username, issues_file, members_file, endpoint)
 
 
 def importIssues(issues, members ):
@@ -50,7 +52,31 @@ def importIssues(issues, members ):
     #print(df.iloc[2])
 
 
-def getAccessToken(endpoint):
-    password = getpass.getpass()
-    response = requests.get(endpoint)
-    print(response)
+def getAccessToken(username, issues_file, members_file, endpoint):
+    print ('username: ', username, ' issues_file: ', issues_file, ' members_file: ', members_file, ' endpoint ', endpoint )
+    if (validators.url(endpoint)):
+        password = getpass.getpass()
+        #response = requests.get(endpoint)
+        d = {}
+        d['password'] = password;
+        d['type'] = 'normal';
+        d['username'] = username;
+        response = requests.post(endpoint + ENDPOINT_LOGIN, data = d)
+        if response.ok:
+            responseJson = response.json()
+            try:
+                global authToken
+                authToken = responseJson['auth_token']
+            except:
+                print ('We could not get the authorization token from the login request')
+                exit (2)
+        else:
+            print ('The response from the server is not valid')
+            exit (2)
+    else:
+        print ('The endpoint provided is not a valid url')
+        exit (2)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
